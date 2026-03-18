51ca183b46d5d74f844e01fcdbdd912faa459d99
pedrolazarine40-bit <pedrolazarine40@gmail.com>
2026-03-17 21:40:04 -0300

    Add files via upload

---
+++ exercicio_2.java
@@ -0,0 +1,7 @@
+public class exercicio_1;{
+    public static void main (sting[] args){
+        int lado = 4
+        int multiplicaçao = lado * lado;
+        System.out.ptintln ("area : " + multiplicaçao);
+    }
+}
