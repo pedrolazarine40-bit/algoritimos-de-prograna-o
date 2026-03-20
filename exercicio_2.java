931e3a7994a15f693f0882e268ed5c8ef80aaaf1
pedrolazarine40-bit <pedrolazarine40@gmail.com>
2026-03-19 21:28:34 -0300

    Delete exercicio_2.java

--- exercicio_2.java
+++
@@ -1,7 +0,0 @@
-public class exercicio_1;{
-    public static void main (sting[] args){
-        int lado = 4
-        int multiplicaçao = lado * lado;
-        System.out.ptintln ("area : " + multiplicaçao);
-    }
-}
